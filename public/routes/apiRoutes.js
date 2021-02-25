8e131e38a25b9a8ee98c4c20a3f5a5de241cc28a
Jas James <jasdjames@gmail.com>
2021-02-25 11:16:08 -0600

    appends note to LI on Notes HTML

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -15,12 +15,14 @@ res.json(noteData)
 
 // // I know I need to export something from index and WriteFileSync so I need a FS
 app.post('/api/notes', (req, res) => {
-    
-const note = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../db/db.json')))
+const dbPath = path.resolve(__dirname, '../../db/db.json')
+const note = JSON.parse(fs.readFileSync(dbPath))
 
 note.push(req.body);
 console.log(note);
-fs.writeFileSync(note);
+const writeNote = fs.writeFileSync(dbPath,JSON.stringify(note));
+console.log('this it is!!',note);
+res.json(note)
 });
 };
 
