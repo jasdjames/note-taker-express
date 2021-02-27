425718eb180966bf9518df94b04acab3fa48934a
Jas James <jasdjames@gmail.com>
2021-02-27 12:03:05 -0600

    added a delete option

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -6,31 +6,45 @@ const { v4: uuid } = require('uuid');
 
 module.exports = (app) => {
 
-app.get('/api/notes', (req, res) => { 
+    app.get('/api/notes', (req, res) => {
+        const dbPath = path.resolve(__dirname, '../../db/db.json')
+        const note = JSON.parse(fs.readFileSync(dbPath))
+        res.json(note)
+    });
+
+
+
+
+    // // I know I need to export something from index and WriteFileSync so I need a FS
+    app.post('/api/notes', (req, res) => {
+        const dbPath = path.resolve(__dirname, '../../db/db.json')
+        const note = JSON.parse(fs.readFileSync(dbPath))
+        req.body['id'] = uuid();
+        note.push(req.body);
+        fs.writeFileSync(dbPath, JSON.stringify(note));
+        console.log('this it is!!', note);
+        res.json(note);
+
+    });
+    
+    app.delete('/api/notes/:id', (req, res) => {
+        console.log('We hit the route');
     const dbPath = path.resolve(__dirname, '../../db/db.json')
     const note = JSON.parse(fs.readFileSync(dbPath))
-res.json(note)
-});
+    console.log('This is note',note);
+    const deleteID = req.params.id;
+    // create a loop that loops through the at checked the  array in the db.json, deleted the value that matched in the array, and then returned the result
+    const newNote = []   
+    for (let i = 0; i < note.length; i++) {
+           if(deleteID !== note[i].id){
+               newNote.push(note[i])
+           }   
+        }
+    fs.writeFileSync(dbPath,JSON.stringify(newNote))
+    res.send('deleted')
+
+}
+)
 
 
-
-
-// // I know I need to export something from index and WriteFileSync so I need a FS
-app.post('/api/notes', (req, res) => {
-const dbPath = path.resolve(__dirname, '../../db/db.json')
-const note = JSON.parse(fs.readFileSync(dbPath))
-req.body['id'] = uuid();
-note.push(req.body);
-fs.writeFileSync(dbPath,JSON.stringify(note));
-console.log('this it is!!',note);
-res.json(note);
-
-});
 };
-
-// app.delete('/api/notes/:id', (req, res) => {
-// const dbPath = path.resolve(__dirname, '../../db/db.json')
-// const note =  JSON.parse(fs.readFileSync(dbPath))
-// const deleteID = req.params.id;
-// }
-// )
